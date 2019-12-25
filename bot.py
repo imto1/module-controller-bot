34f5956a93a292e0f162560d8e1a2cc97f34b002
imto1 <imto1.mail@gmail.com>
2019-12-25 04:07:39 -0500

    get username

--- bot.py
+++ bot.py
@@ -124,7 +124,7 @@ def excecute(command):
 def start_bot(chat):
 	user = chat["username"]
 	name = chat["first_name"]
-	send_message("Welcome {}!\nIOT module controller for {}.".format(name, user), chat)
+	send_message("Welcome {}!\nIOT module controller for {}.".format(name, user), chat["id"])
     #login(chat)
 
     
